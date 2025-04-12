abcc7674bf822788f3970d8dc1975ca99adb6401
Due-Ad <joeksztein@gmail.com>
2025-04-12 00:07:03 -0300

    Update main.c

--- main.c
+++ main.c
@@ -1,8 +1,31 @@
 #include <stdio.h>
 
+int fibonacci(int);
+
 int main()
 {
+    int n;
+
     printf("Hello, world!");
 
+    printf("Ingrese n, numero natural, para calcular la secuencia de fibonacci hasta su n-avo paso\n");
+    scanf("%d", &n);
+
+    printf("El %d-avo numero de la secuencia de fibonacci es: %d\m", fibonacci(n));
+
     return 0;
+}
+
+int fibonacci(int n)
+{
+    int a = 1, b = 0, c = 0, k = 0;
+
+    for(k = 0; k < n; k++)
+    {
+        c = b + a;
+        a = b; 
+        b = c;
+    }
+
+    return a;
 }
